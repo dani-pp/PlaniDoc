863ebfc55a9e5451008f2c996949aeed94c3c787
dani-pp <pesodaniza@gmail.com>
2025-06-25 17:02:39 -0400

    Inicialiacion tailwind

--- app.py
+++ app.py
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/agendar")
+def agendar():
+    return render_template("agendar.html")
+
+@app.route("/iniciar-sesion")
+def iniciar():
+    return render_template("iniciar_sesion.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
