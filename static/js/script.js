450d51bdf9f08e30d153fe3629fafb30eecd4b77
dani-pp <pesodaniza@gmail.com>
2025-06-27 10:34:29 -0400

    animaciones seccion 2 landing page

---
+++ static/js/script.js
@@ -0,0 +1,34 @@
+
+ScrollReveal().reveal('.titulo-segunda-seccion', {
+  origin: 'left',
+  distance: '30px',
+  duration: 1100,
+  delay: 100,
+  reset: true
+});
+
+
+ScrollReveal().reveal('.elemento-1', {
+  origin: 'bottom',
+  distance: '40px',
+  duration: 1200,
+  delay: 300,
+  reset: true
+});
+
+ScrollReveal().reveal('.elemento-2', {
+  origin: 'bottom',
+  distance: '40px',
+  duration: 1200,
+  delay: 800,
+  reset: true
+});
+
+ScrollReveal().reveal('.elemento-3', {
+  origin: 'bottom',
+  distance: '40px',
+  duration: 1200,
+  delay: 1300,
+  reset: true
+});
+
